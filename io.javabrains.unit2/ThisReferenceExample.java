a43085d501e1592a92908d59d72d66c6ffa212bb
Jeffreylsq <40809623+Jeffreylsq@users.noreply.github.com>
2018-10-01 08:24:22 -0700

    CS370--8

---
+++ io.javabrains.unit2/ThisReferenceExample.java
@@ -0,0 +1,55 @@
+
+public class ThisReferenceExample {
+
+	public void doProcess(int i, Process p) {
+		
+		p.process(i);
+	}
+	
+	
+	public void execute()
+	{
+		doProcess(10, i->{
+			System.out.println("Value of i is " + i);
+			System.out.println(this);
+		});
+	}
+	
+	
+	
+	public static void main(String[] args) {
+	
+	ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
+	
+	thisReferenceExample.execute();
+	
+	
+/*  thisReferenceExample.doProcess(10, i->{
+	System.out.println("Value of is" + i);
+	//System.out.println(this);
+});
+	
+		
+	*/	
+	
+	
+	
+	/*thisReferenceExample.doProcess(10, new Process() {
+		
+		@Override
+		public void process(int i)
+		{
+			System.out.println("Value of i is" + i);
+			System.out.println(this);
+		}
+		
+		public String toString()
+		{
+			return "This is the anonymous inner class";
+		}
+		
+	});
+ */
+	}
+
+}
